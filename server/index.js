be300e9d63d3f390293e045e6f25e6ead68c702f
Shreya Ghate <shreya.ghate.sg@gmail.com>
2021-01-21 06:44:00 +0000

    Add: CORS Headers

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const debug = require("debug")("server");
 const app = express();
 const port = process.env.SERVER_PORT || 3001;
 
+//Add CORS Headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Max-Age", "86400");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, HEAD" ); 
+  next();
+});
+
 app.get("/api/ping", (req, res) => {
   res.send({
     msg: "Hello, World"
